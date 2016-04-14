3649acf3bb7e0c4b980b7e73a7e08bfe10ce2337
Soowon Kang <cubist.kang@gmail.com>
2016-04-15 00:04:09 +0900

    modify marriage

--- src/python/etc/marriage.py
+++ src/python/etc/marriage.py
@@ -1,11 +1,14 @@
 # This is HW for stable-matching algorithm.
 
-def stable_matching(man=[], woman=[]):
-    assert instance(man, list) or instance(man, tuple)
-    assert instance(woman, list) or instance(man, tuple)
+def stable_matching(men=[], women=[]):
+    assert isinstance(men, list) or isinstance(men, tuple)
+    assert isinstance(women, list) or isinstance(women, tuple)
     
     matched = []
     return matched
+
+def verify_matching(matched_list=[], men=[], women=[]):
+    pass
 
 if __name__ == "__main__":
     m = ((3,2,1),(3,2,1),(3,2,1))
